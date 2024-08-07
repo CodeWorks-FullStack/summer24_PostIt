b1305b3c98931f5694fb2b0c35ed1461555da2fd
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-07 14:12:20 -0600

    ⚠️ADDING SOCKETS TO REPO⚠️

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'mickshanny.us.auth0.com'
-export const clientId = 'luwNBP378EfKtXGGgxJj9T2kNZ1A562I'
-export const audience = 'http://Sandbox.com'
+export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
+export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
+export const audience = 'https://jeremyisaraddude.com'
