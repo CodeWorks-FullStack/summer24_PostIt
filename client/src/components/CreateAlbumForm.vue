da97ebd086ebb2e569f1087c3209543e84007319
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-07 14:15:04 -0600

    SOCKETS ARE ON

--- client/src/components/CreateAlbumForm.vue
+++ client/src/components/CreateAlbumForm.vue
@@ -11,71 +11,72 @@ const router = useRouter() // is the vehicle that moves you from place to place
 
 
 const albumData = ref({
-title: '',
-coverImg: '',
-description: undefined,
-category: ''
+  title: '',
+  coverImg: '',
+  description: undefined,
+  category: ''
 })
 
-async function createAlbum(){
+async function createAlbum() {
   try {
     const newAlbum = await albumsService.createAlbum(albumData.value)
     Pop.success(`You did it yay! you created ${newAlbum.title} `)
     resetForm()
     Modal.getOrCreateInstance('#create-album-modal').hide()
-    router.push({name: 'Album Details', params: {albumId: newAlbum.id}})
+    // router.push({name: 'Album Details', params: {albumId: newAlbum.id}})
   } catch (error) {
     Pop.toast('Could not create album', 'error', 'center-start')
     logger.error(error)
   }
 }
 
-function resetForm(){
+function resetForm() {
   albumData.value = {
-title: '',
-coverImg: '',
-description: undefined,
-category: ''
-}
+    title: '',
+    coverImg: '',
+    description: undefined,
+    category: ''
+  }
 }
 
 </script>
 
 
 <template>
-<form class="container-fluid" @submit.prevent="createAlbum()">
-  <p class="fs-2">Create an Album</p>
-  <section class="row">
-    <div class="mb-2 col-6">
-      <label for="album-title">Title</label>
-      <input v-model="albumData.title" class="form-control" type="text" id="album-title" name="album-title" minlength="3" maxlength="25" required>
-    </div>
-    <div class="mb-2 col-6">
-      <label for="album-cover-img">Cover Image</label>
-      <input v-model="albumData.coverImg" class="form-control" type="url" id="album-cover-img" name="album-cover-img" required>
-    </div>
-    <div class="mb-2 col-12">
-      <label for="album-description">Description</label>
-      <textarea v-model="albumData.description" minlength="15" maxlength="250" class="form-control" name="album-description" id="album-description"></textarea>
-    </div>
-    <div class="mb-2 col-12">
-      <label for="album-category">Category</label>
-      <select v-model="albumData.category" required class="form-control" name="album-category" id="album-category">
-        <option value="" disabled>Please Select A Category</option>
-        <option value="animals">Animals</option>
-        <option value="games">Games</option>
-        <option value="misc">Miscellaneous</option>
-      </select>
-    </div>
-    <div class="col-12 text-end">
-      <button type="button" @click="resetForm()"> clear </button>
-      <button class="btn btn-success">Submit</button>
-    </div>
-  </section>
-</form>
+  <form class="container-fluid" @submit.prevent="createAlbum()">
+    <p class="fs-2">Create an Album</p>
+    <section class="row">
+      <div class="mb-2 col-6">
+        <label for="album-title">Title</label>
+        <input v-model="albumData.title" class="form-control" type="text" id="album-title" name="album-title"
+          minlength="3" maxlength="25" required>
+      </div>
+      <div class="mb-2 col-6">
+        <label for="album-cover-img">Cover Image</label>
+        <input v-model="albumData.coverImg" class="form-control" type="url" id="album-cover-img" name="album-cover-img"
+          required>
+      </div>
+      <div class="mb-2 col-12">
+        <label for="album-description">Description</label>
+        <textarea v-model="albumData.description" minlength="15" maxlength="250" class="form-control"
+          name="album-description" id="album-description"></textarea>
+      </div>
+      <div class="mb-2 col-12">
+        <label for="album-category">Category</label>
+        <select v-model="albumData.category" required class="form-control" name="album-category" id="album-category">
+          <option value="" disabled>Please Select A Category</option>
+          <option value="animals">Animals</option>
+          <option value="games">Games</option>
+          <option value="misc">Miscellaneous</option>
+        </select>
+      </div>
+      <div class="col-12 text-end">
+        <button type="button" @click="resetForm()"> clear </button>
+        <button class="btn btn-success">Submit</button>
+      </div>
+    </section>
+  </form>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
